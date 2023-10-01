87d2730609421a60bc825e2568f3da513acb807a
Gusttavo-costta <gccostta23@gmail.com>
2023-10-01 13:15:57 -0300

    :sparkles:

--- src/App.jsx
+++ src/App.jsx
@@ -67,11 +67,11 @@ function App() {
     <div className="app">
       <h1>Lista de tarefas</h1>
       <Search search={search} setSearch={setSearch}/>
-      <Filter filter={filter} setFilter={setFilter}/>
+      <Filter filter={filter} setFilter={setFilter} setSort={setSort}/>
       <div className="todo-list">
         {todos
         .filter((todo) => filter === "All" ? true : filter === "Completed" ? todo.isCompleted : !todo.isCompleted)
-        .filter((todo) => todo.text.toLowerCase().includes(search.toLowerCase())).map((todo) => (
+        .filter((todo) => todo.text.toLowerCase().includes(search.toLowerCase())).sort((a, b) => sort === "Asc" ? a.text.localeCompare(b.text) : b.text.localeCompare(a.text)).map((todo) => (
           <Todo key={todo.id} todo={todo} removeTodo={removeTodo} completeTodo={completeTodo}/>
         ))}
       </div>
